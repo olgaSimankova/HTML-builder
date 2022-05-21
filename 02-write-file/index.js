af9a250d1870ab5bc61f0d85fc31d166e81d99ac
olgaSimankova <o.kotlyarova@gmail.com>
2022-05-21 22:06:23 +0300

    feat: add 02-write-file solution. Finally (05/21/2022 22:06:23)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,27 @@
 const fs = require('fs')
 const readline = require('readline')
-// const stream = require('stream')
+const path = require('path')
 
-const { stdin: input, stdout: output, stderr } = require('process');
-const rl = readline.createInterface({ input, output })
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+const stdout = process.stdout
+fs.createWriteStream(path.join(__dirname, 'text.txt'))
 
-rl.question('Привет! Напечатай что-нибудь:\n', (answer) => {
-        const output = fs.createWriteStream('./02-write-file/text.txt')
-        if (answer.toString().trim().includes('exit')) {
-            rl.close()
-            console.log("Ну пока :)")
-        } else {
-        output.write(answer + '\n')
-    }
-})
+const writeToFile = () => {
+    stdout.write('Привет! Напечатай что-нибудь:\n')
+    rl.on('line', (text) => {
+        if (text.trim()=='exit') {
+            console.log("Ну пока :) Уачи!")
+            process.exit()
+        }
+      fs.appendFile(path.join(__dirname, 'text.txt'), text + '\n', (err) => {
+        if (err) return
+      })
+    })
+  }
+
+writeToFile()
 
 rl.on("SIGINT", () => {
-    rl.close()
-    console.log("Ну пока :)")
+    console.log("Ну пока :) Уачи!")
+    process.exit()
 })
